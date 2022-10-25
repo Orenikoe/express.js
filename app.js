79e6b385c054489750ee13f2ea6c0abe563e2ccd
Orenikoe <95924002+Orenikoe@users.noreply.github.com>
2022-10-25 11:32:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const port = process.env.APP_PORT ?? 5000;
+
+const express = require("express");
+
+const app = express();
+
+const welcome = (req, res) => {
+  res.send("Welcoe to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+// app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
